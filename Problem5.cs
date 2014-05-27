0d5de283a64a88752a6bca1ea7c5315565c7446e
Neil Opet <neil.opet@gmail.com>
2014-05-26 23:46:37 -0400

    Removed useless namespace call

--- Problem5.cs
+++ Problem5.cs
@@ -4,7 +4,6 @@
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  */
 using System;
-using System.Collections.Generic;
 
 public class Problem5
 {
