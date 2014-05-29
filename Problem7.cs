780979b1f7f1c8cd3e27fa763c732cec521a8b34
Neil Opet <neil.opet@gmail.com>
2014-05-28 23:04:41 -0400

    Update Problem7.cs

--- Problem7.cs
+++ Problem7.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 
 public class Problem7
 {
-	public void Main()
+	public static void Main()
 	{
 		Console.WriteLine(GeneratePrimesSieveOfEratosthenes(10001));
 	}
@@ -18,58 +18,57 @@ public class Problem7
 	Credit: http://stackoverflow.com/a/1072205 */
 	public static int ApproximateNthPrime(int nn)
 	{
-	    double n = (double)nn;
-	    double p;
-	    if (nn >= 7022)
-	    {
-	        p = n * Math.Log(n) + n * (Math.Log(Math.Log(n)) - 0.9385);
-	    }
-	    else if (nn >= 6)
-	    {
-	        p = n * Math.Log(n) + n * Math.Log(Math.Log(n));
-	    }
-	    else if (nn > 0)
-	    {
-	        p = new int[] { 2, 3, 5, 7, 11 }[nn - 1];
-	    }
-	    else
-	    {
-	        p = 0;
-	    }
-	    return (int)p;
+		double n = (double)nn;
+		double p;
+		if (nn >= 7022)
+		{
+			p = n * Math.Log(n) + n * (Math.Log(Math.Log(n)) - 0.9385);
+		}
+		else if (nn >= 6)
+		{
+			p = n * Math.Log(n) + n * Math.Log(Math.Log(n));
+		}
+		else if (nn > 0)
+		{
+			p = new int[] { 2, 3, 5, 7, 11 }[nn - 1];
+		}
+		else
+		{
+			p = 0;
+		}
+		return (int)p;
 	}
 
-	public static BitArray SieveOfEratosthenes(int limit)
+	public static bool[] SieveOfEratosthenes(int limit)
 	{
-	    BitArray bits = new BitArray(limit + 1, true);
-	    bits[0] = false;
-	    bits[1] = false;
-	    for (int i = 0; i * i <= limit; i++)
-	    {
-	        if (bits[i])
-	        {
-	            for (int j = i * i; j <= limit; j += i)
-	            {
-	                bits[j] = false;
-	            }
-	        }
-	    }
-	    return bits;
+		bool[] bits = new bool[limit + 1];
+		for( int i = 0; i < bits.Length; bits[i++] = true);
+		
+		bits[0] = false;
+		bits[1] = false;
+		for (int i = 0, trueLimit = (int)Math.Sqrt(limit); i <= trueLimit; i++)
+		{			
+			if (bits[i])
+			{
+				for (int j = i * i; j <= limit; bits[j] = false, j += i);
+			}
+		}
+		return bits;
 	}
 
 	public static int GeneratePrimesSieveOfEratosthenes(int n)
 	{
-	    int largestPrime = 0;
-	    int limit = ApproximateNthPrime(n);
-	    BitArray bits = SieveOfEratosthenes(limit);
-	    for (int i = 0, found = 0; i < limit && found < n; i++)
-	    {
-	        if (bits[i] && i > largestPrime)
-	        {
+		int largestPrime = 0;
+		int limit = ApproximateNthPrime(n);
+		bool[] bits = SieveOfEratosthenes(limit);
+		for (int i = 0, found = 0; i < limit && found < n; i++)
+		{
+			if (bits[i] && i > largestPrime)
+			{
 				largestPrime = i;
-	            found++;
-	        }
-	    }
-	    return largestPrime;
+				found++;
+			}
+		}
+		return largestPrime;
 	}
 }
