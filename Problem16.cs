80d5725e9b68026ae9fce2701ad95589fa9e1bb9
Neil Opet <neil.opet@gmail.com>
2014-06-03 22:36:19 -0400

    Create Problem16.cs

---
+++ Problem16.cs
@@ -0,0 +1,24 @@
+/*
+ * Problem 16
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+ */
+using System;
+using System.Numerics;
+
+public class Problem16
+{
+	public static void Main()
+	{
+	 /* Left bit shifting is the same as raising to power of 2 */
+		BigInteger n = new BigInteger(1);
+		n<<=1000;
+		String s = n.ToString();
+		int sum = 0;
+		for(int i = 0, j = s.Length; i < j; sum += Int32.Parse(s.Substring(i, 1)), i++)
+		;
+		Console.WriteLine(sum);
+	}
+	
+}
